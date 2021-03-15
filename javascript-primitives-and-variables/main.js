e50508d56f6a673a24becc29e2ba618adf22fc25
Jake <jdfenlason@gmail.com>
2021-03-15 14:17:03 -0400

    fixed javascript-primitives-and-variables

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -8,3 +8,24 @@ var totalPets = 2;
 var unknown = null;
 
 var never;
+
+
+console.log("value of fullName:", fullName);
+
+console.log("value of isCool:", isCool);
+
+console.log('value of totalPets:', totalPets);
+
+console.log("First Name:", typeof fullName);
+
+console.log("typeof isCool:", typeof isCool);
+
+console.log("typeof totalPets:", typeof totalPets);
+
+console.log("value of unkown:", unknown);
+
+console.log("typeof unknown:", typeof unknown);
+
+console.log("value of never:", never);
+
+console.log("typeof never:", typeof never);

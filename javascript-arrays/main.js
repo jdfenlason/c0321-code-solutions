e5f17d910984274af21dac8ced7e528ba5a93ca6
Jake <jdfenlason@gmail.com>
2021-03-15 21:26:28 -0400

    completed javascript-arrays

---
+++ javascript-arrays/main.js
@@ -0,0 +1,29 @@
+var colors = ['red', 'white', 'blue'];
+
+console.log('Value of index[0]:', colors[0]);
+console.log("Value of index[1]:", colors[1]);
+console.log("Value of index[2]:", colors[2]);
+
+console.log('America is' + ' ' + colors[0], colors[1], 'and'+ ' ' + colors[2]);
+
+colors[2] = 'green';
+
+console.log("Mexico is" + " " + colors[0], colors[1], "and" + " " + colors[2]);
+
+console.log('Value of colors:', colors);
+
+var students = ['Larry', 'John', 'Carmen', 'Gary'];
+
+var numberOfStudents = students.length;
+
+console.log('There are ' + numberOfStudents + ' students in the class');
+
+var lastIndex = numberOfStudents - 1;
+
+
+
+var lastStudent = students[lastIndex];
+
+console.log('The last student in the array is ',  lastStudent);
+
+console.log('value of Students:', students)

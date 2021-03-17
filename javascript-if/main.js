50e4d501dd669726ecc94348bf8ec1b0afa25f0b
Jake <jdfenlason@gmail.com>
2021-03-17 19:24:14 -0400

    fixed javascript-if

--- javascript-if/main.js
+++ javascript-if/main.js
@@ -8,97 +8,95 @@
             introduceWarnerBro
  */
 var shia = {
-  name: 'Shia Lebouf',
-  age: 34
+  name: "Shia Lebouf",
+  age: 34,
 };
-
 
 function isOldEnoughToDrink(age) {
-
-  if (age => 21) {
+  if ((age) => 21) {
     return true;
   } else {
-    return
-    (false)
-  };
-
+    return;
+    false;
+  }
 };
 
-
 function isOldEnoughToDrive(age) {
-  if (age => 18) {
-    return true
-    } else {
-      return false;
-    }
-  };
-
-
-  function isOldEnoughToDrinkAndDrive(age) {
-    return false;
-  };
-
-  function isUnderFive(number) {
-    if (number <= 5) {
-      return true;
-    } else {
-      return false
-    }
-  };
-
-  function isEven(number) {
-    if (number % 2 === 0) {
-      return true
-    } else {
-      return false
-    }
-  };
-  function startsWithJ(string) {
-    if(string[0] === 'J') {
-      return true
-    } else {
-        return false
-      }
-    };
-
-    function isOldEnoughToDrink(person) {
-      if(person => 21) {
-        return true
-      } else {
-        return false
-      }
-    };
-    function isOldEnoughToDrive(person) {
-      if(person => 18) {
-        return true
-      } else {
-        return false
-      }
-    };
-
-      function isOldEnoughToDrinkAndDrive(person) {
-    return false;
-  };
-
-  function categorizeAcidity(ph) {
-    if(ph === 7) {
-      return 'neutral'
-    } if (ph < 7 && ph > 0){
-     return 'acid'
-    } if(ph > 8 && ph < 14) {
-    return 'base'
+  if ((age) => 18) {
+    return true;
   } else {
-    return 'invalid ph level'
+    return false;
+  }
+};
+
+function isOldEnoughToDrinkAndDrive(age) {
+  return false;
+};
+
+function isUnderFive(number) {
+  if (number <= 5) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function startsWithJ(string) {
+  if (string[0] === "J") {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function isOldEnoughToDrink(person) {
+  if ((person) => 21) {
+    return true;
+  } else {
+    return false;
+  }
+};
+function isOldEnoughToDrive(person) {
+  if ((person) => 18) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function isOldEnoughToDrinkAndDrive(person) {
+  return false;
+};
+
+function categorizeAcidity(ph) {
+  if (ph === 7) {
+    return "neutral";
+  }
+  if (ph < 7 && ph > 0) {
+    return "acid";
+  }
+  if (ph > 8 && ph < 14) {
+    return "base";
+  } else {
+    return "invalid ph level";
   }
 };
 
 function introduceWarnerBro(name) {
-   if(name=== 'yakko' || name=== 'wakko') {
- return "We're the warner brothers"
-   } if(name === 'dot') {
-    return "I'm cute~"
+  if (name === "yakko" || name === "wakko") {
+    return "We're the warner brothers";
   }
-  else {
-    return "Goodnight everybody"
-}
+  if (name === "dot") {
+    return "I'm cute~";
+  } else {
+    return "Goodnight everybody";
+  }
 };

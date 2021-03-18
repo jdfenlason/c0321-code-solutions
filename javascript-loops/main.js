9adcb405191ccdc157f6353edc8940046282a57f
Jake <jdfenlason@gmail.com>
2021-03-18 11:17:40 -0400

    completed javascript-loops

--- javascript-loops/main.js
+++ javascript-loops/main.js
@@ -36,10 +36,32 @@ function repeatWord(word, times) {
   return repeated;
 }
 
+function logEachCharacter(string) {
+  for (var i = 0; i < string.length; i++) {
+    console.log(string[i]);
+  }
+}
+
 function doubleAll(numbers) {
   var doubled = [];
   for (var i = 0; i < numbers.length; i++) {
     doubled.push(numbers[i] * 2);
   }
   return doubled;
+}
+
+function getKeys(object) {
+  var keys = [];
+  for (var key in object) {
+    keys.push(key);
+  }
+  return keys;
+}
+
+function getValues(object) {
+  var values = [];
+  for (var key in object) {
+    values.push(object[key]);
+  }
+  return values;
 }

b03b4fb595804cde9609befb2c8748b114bc855d
Jake <jdfenlason@gmail.com>
2021-03-18 20:31:30 -0400

    fixed javascript-if

--- javascript-if/main.js
+++ javascript-if/main.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-redeclare */
+/* eslint-disable no-unused-vars */
 /* exported isUnderFive,
             isEven,
             startsWithJ,
@@ -6,40 +8,7 @@
             isOldEnoughToDrinkAndDrive,
             categorizeAcidity,
             introduceWarnerBro
- */
-var shia = {
-  name: "Shia Lebouf",
-  age: 34,
-};
-
-function isOldEnoughToDrink(age) {
-  if ((age) => 21) {
-    return true;
-  } else {
-    return;
-    false;
-  }
-}
-
-function isOldEnoughToDrive(age) {
-  if ((age) => 18) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function isOldEnoughToDrinkAndDrive(age) {
-  return false;
-}
-
-function isUnderFive(number) {
-  if (number <= 5) {
-    return true;
-  } else {
-    return false;
-  }
-}
+            */
 
 function isEven(number) {
   if (number % 2 === 0) {
@@ -48,24 +17,16 @@ function isEven(number) {
     return false;
   }
 }
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 function startsWithJ(string) {
-  if (string[0] === "J") {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function isOldEnoughToDrink(person) {
-  if ((person) => 21) {
-    return true;
-  } else {
-    return false;
-  }
-}
-function isOldEnoughToDrive(person) {
-  if ((person) => 18) {
+  if (string[0] === 'J') {
     return true;
   } else {
     return false;
@@ -78,25 +39,41 @@ function isOldEnoughToDrinkAndDrive(person) {
 
 function categorizeAcidity(ph) {
   if (ph === 7) {
-    return "neutral";
+    return 'neutral';
   }
   if (ph < 7 && ph > 0) {
-    return "acid";
+    return 'acid';
   }
-  if (ph > 8 && ph < 14) {
-    return "base";
+  if (ph >= 8 && ph <= 14) {
+    return 'base';
   } else {
-    return "invalid ph level";
+    return 'invalid ph level';
+  }
+}
+
+function isOldEnoughToDrink(person) {
+  if (person.age >= 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrive(person) {
+  if (person.age >= 18) {
+    return true;
+  } else {
+    return false;
   }
 }
 
 function introduceWarnerBro(name) {
-  if (name === "yakko" || name === "wakko") {
+  if (name === 'yakko' || name === 'wakko') {
     return "We're the warner brothers";
   }
-  if (name === "dot") {
+  if (name === 'dot') {
     return "I'm cute~";
   } else {
-    return "Goodnight everybody";
+    return 'Goodnight everybody';
   }
 }

46007586f3ec1af8fc9b6bb5cbfdc865ecb716ea
Jake <jdfenlason@gmail.com>
2021-03-25 01:25:42 -0400

    fixed javascript-modal

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -2,15 +2,15 @@ var $openBtn = document.querySelector('.modalBtn');
 var $modal = document.querySelector('.modal');
 var $container = document.querySelector('.container');
 
-$openBtn.addEventListener('click', function event() {
-  $container.classList.toggle('active');
-  $modal.style.display = 'flex';
+$openBtn.addEventListener('click', function (e) {
+  $modal.classList.toggle('active');
   $openBtn.classList.toggle('inactive');
+  $container.classList.toggle('active');
 });
 
 var $nobtn = document.querySelector('.nobtn');
-$nobtn.addEventListener('click', function e() {
-  $modal.style.display = 'none';
+$nobtn.addEventListener('click', function (e) {
+  $modal.classList.toggle('active');
   $openBtn.classList.toggle('inactive');
   $container.classList.toggle('active');
 });

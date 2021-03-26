6223c37a9df8670ea4599d653217a98d55f57c9f
Jake <jdfenlason@gmail.com>
2021-03-26 18:22:01 -0400

    completed javascript-and-json

---
+++ javascript-and-json/main.js
@@ -0,0 +1,28 @@
+var myArr = [
+  {
+    isbn: '121551-85858',
+    title: 'Harry Potter and the Order of Pheonix',
+    Author: 'J.K Rowling'
+  },
+  {
+    isbn: '121551-87787',
+    title: 'Harry Potter and Deathly Hallows',
+    Author: 'J.K Rowling'
+  },
+  {
+    isbn: '121551-585858',
+    title: 'Harry Potter and the Half-Blood Prince',
+    Author: 'J.K Rowling'
+  }
+];
+
+console.log('My array of books: ', myArr);
+console.log('Type of myArray ', typeof myArr);
+
+console.log('   JSON string of book array: ', JSON.stringify(myArr));
+console.log('Type of JSON string: ', typeof JSON.stringify(myArr));
+
+console.log('String in JSON FORM with single quotes ', '{ "name":"John", "Number ID": 37883}');
+console.log('Student typeof ', typeof '{ "name":"John", "Number ID": 37883}');
+
+console.log('JSON.Parse: ', JSON.parse('{ "name":"John", "Number ID": 37883}'));

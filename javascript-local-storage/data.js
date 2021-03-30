1089239175595401e8c47ce6525556323f4f7f87
Jake <jdfenlason@gmail.com>
2021-03-30 11:40:47 -0400

    Fixed and added if null statement

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -1,9 +1,11 @@
 /* exported todos */
 
 var todos = [];
-// eslint-disable-next-line no-unused-vars
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
-todos = (JSON.parse(localStorage.getItem('javascript-local-storage')));
+
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
 
 window.addEventListener('beforeunload', function (event) {
   var todosJSON = JSON.stringify(todos);

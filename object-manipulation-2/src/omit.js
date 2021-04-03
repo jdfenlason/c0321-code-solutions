c250f95b86c53eadf04f10a49d702b76071dce8b
Jake <jdfenlason@gmail.com>
2021-04-03 09:35:09 -0400

    Completed object-manipulation-2

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,10 @@
 /* exported omit */
+function omit(source, keys) {
+  var omitedObject = {};
+  for (var key in source) {
+    if (!keys.includes(key)) {
+      omitedObject[key] = source[key];
+    }
+  }
+  return omitedObject;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,10 @@
 /* exported pick */
+function pick(source, keys) {
+  var pickObj = {};
+  for (var key in source) {
+    if (keys.includes(key) && source[key] !== undefined) {
+      pickObj[key] = source[key];
+    }
+  }
+  return pickObj;
+}

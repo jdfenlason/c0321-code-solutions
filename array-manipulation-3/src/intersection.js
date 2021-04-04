7189f8ba45ff8326fcafe1e6ee644fd90789c928
Jake <jdfenlason@gmail.com>
2021-04-04 10:41:22 -0400

    Completed array-manipulation-3

---
+++ array-manipulation-3/src/intersection.js
@@ -0,0 +1,10 @@
+/* exported intersection */
+function intersection(first, second) {
+  var newArr = [];
+  for (var i = 0; i < first.length; i++) {
+    if (second.includes(first[i])) {
+      newArr.push(first[i]);
+    }
+  }
+  return newArr;
+}
---
+++ array-manipulation-3/src/zip.js
@@ -0,0 +1,14 @@
+/* exported zip */
+function zip(first, second) {
+  var newArr = [];
+  if (first.length === second.length || first.length < second.length) {
+    for (var i = 0; i < first.length; i++) {
+      newArr.push([first[i], second[i]]);
+    }
+  } else if (first.length > second.length) {
+    for (var k = 0; k < second.length; k++) {
+      newArr.push([first[k], second[k]]);
+    }
+  }
+  return newArr;
+}

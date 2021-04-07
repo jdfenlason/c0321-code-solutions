4ebbe264f6ea8667e3d34d66670f6a0653896324
Jake <jdfenlason@gmail.com>
2021-04-07 16:02:50 -0400

    Fixed zip conditionals in array-manipulation-3

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,11 +1,11 @@
 /* exported zip */
 function zip(first, second) {
   var newArr = [];
-  if (first.length === second.length || first.length < second.length) {
+  if (first.length <= second.length) {
     for (var i = 0; i < first.length; i++) {
       newArr.push([first[i], second[i]]);
     }
-  } else if (first.length > second.length) {
+  } else {
     for (var k = 0; k < second.length; k++) {
       newArr.push([first[k], second[k]]);
     }

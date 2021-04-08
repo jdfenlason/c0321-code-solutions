6336a58ff07cdadd6cea96a2ff4f4308f0a1037b
Jake <jdfenlason@gmail.com>
2021-04-08 17:31:28 -0400

    in progress object-manipulation-3

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,241 +1,246 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
-// var players = [
+// function cardGame (players, cards) {
+//   //shuffle
+// var _.shuffle
+// }
 
-//   {
-//     name: 'Boulder',
-//     hand: []
-//   },
-//   {
-//     name: 'Thor',
-//     hand: []
-//   },
+var players = [
 
-//   {
-//     name: 'Odin',
-//     hand: []
-//   },
+  {
+    name: 'Balder',
+    hand: []
+  },
+  {
+    name: 'Thor',
+    hand: []
+  },
 
-//   {
-//     name: 'Loki',
-//     hand: []
-//   }
-// ];
+  {
+    name: 'Odin',
+    hand: []
+  },
 
-// var cards = [
-//   {
-//     suit: 'spades',
-//     rank: 'ace'
-//   },
-//   {
-//     suit: 'spades',
-//     rank: 'king'
-//   },
-//   {
-//     suit: 'spades',
-//     rank: 'queen'
-//   },
-//   {
-//     suit: 'spades',
-//     rank: 'jack'
-//   },
-//   {
-//     suit: 'spades',
-//     rank: 10
-//   },
-//   {
-//     suit: 'spades',
-//     rank: 9
-//   },
-//   {
-//     suit: 'spades',
-//     rank: 8
-//   },
-//   {
-//     suit: 'spades',
-//     rank: 7
-//   },
-//   {
-//     suit: 'spades',
-//     rank: 6
-//   },
-//   {
-//     suit: 'spades',
-//     rank: 5
-//   },
-//   {
-//     suit: 'spades',
-//     rank: 4
-//   },
-//   {
-//     suit: 'spades',
-//     rank: 3
-//   },
-//   {
-//     suit: 'spades',
-//     rank: 2
-//   },
+  {
+    name: 'Loki',
+    hand: []
+  }
+];
 
-//   {
-//     suit: 'clubs',
-//     rank: 'ace'
-//   },
-//   {
-//     suit: 'clubs',
-//     rank: 'king'
-//   },
-//   {
-//     suit: 'clubs',
-//     rank: 'queen'
-//   },
-//   {
-//     suit: 'clubs',
-//     rank: 'jack'
-//   },
-//   {
-//     suit: 'clubs',
-//     rank: 10
-//   },
-//   {
-//     suit: 'clubs',
-//     rank: 9
-//   },
-//   {
-//     suit: 'clubs',
-//     rank: 8
-//   },
-//   {
-//     suit: 'clubs',
-//     rank: 7
-//   },
-//   {
-//     suit: 'clubs',
-//     rank: 6
-//   },
-//   {
-//     suit: 'clubs',
-//     rank: 5
-//   },
-//   {
-//     suit: 'clubs',
-//     rank: 4
-//   },
-//   {
-//     suit: 'clubs',
-//     rank: 3
-//   },
-//   {
-//     suit: 'clubs',
-//     rank: 2
-//   },
+var cards = [
+  {
+    suit: 'spades',
+    rank: 'ace'
+  },
+  {
+    suit: 'spades',
+    rank: 'king'
+  },
+  {
+    suit: 'spades',
+    rank: 'queen'
+  },
+  {
+    suit: 'spades',
+    rank: 'jack'
+  },
+  {
+    suit: 'spades',
+    rank: 10
+  },
+  {
+    suit: 'spades',
+    rank: 9
+  },
+  {
+    suit: 'spades',
+    rank: 8
+  },
+  {
+    suit: 'spades',
+    rank: 7
+  },
+  {
+    suit: 'spades',
+    rank: 6
+  },
+  {
+    suit: 'spades',
+    rank: 5
+  },
+  {
+    suit: 'spades',
+    rank: 4
+  },
+  {
+    suit: 'spades',
+    rank: 3
+  },
+  {
+    suit: 'spades',
+    rank: 2
+  },
 
-//   {
-//     suit: 'hearts',
-//     rank: 'ace'
-//   },
-//   {
-//     suit: 'hearts',
-//     rank: 'king'
-//   },
-//   {
-//     suit: 'hearts',
-//     rank: 'queen'
-//   },
-//   {
-//     suit: 'hearts',
-//     rank: 'jack'
-//   },
-//   {
-//     suit: 'hearts',
-//     rank: 10
-//   },
-//   {
-//     suit: 'hearts',
-//     rank: 9
-//   },
-//   {
-//     suit: 'hearts',
-//     rank: 8
-//   },
-//   {
-//     suit: 'hearts',
-//     rank: 7
-//   },
-//   {
-//     suit: 'hearts',
-//     rank: 6
-//   },
-//   {
-//     suit: 'hearts',
-//     rank: 5
-//   },
-//   {
-//     suit: 'hearts',
-//     rank: 4
-//   },
-//   {
-//     suit: 'hearts',
-//     rank: 3
-//   },
-//   {
-//     suit: 'hearts',
-//     rank: 2
-//   },
+  {
+    suit: 'clubs',
+    rank: 'ace'
+  },
+  {
+    suit: 'clubs',
+    rank: 'king'
+  },
+  {
+    suit: 'clubs',
+    rank: 'queen'
+  },
+  {
+    suit: 'clubs',
+    rank: 'jack'
+  },
+  {
+    suit: 'clubs',
+    rank: 10
+  },
+  {
+    suit: 'clubs',
+    rank: 9
+  },
+  {
+    suit: 'clubs',
+    rank: 8
+  },
+  {
+    suit: 'clubs',
+    rank: 7
+  },
+  {
+    suit: 'clubs',
+    rank: 6
+  },
+  {
+    suit: 'clubs',
+    rank: 5
+  },
+  {
+    suit: 'clubs',
+    rank: 4
+  },
+  {
+    suit: 'clubs',
+    rank: 3
+  },
+  {
+    suit: 'clubs',
+    rank: 2
+  },
 
-//   {
-//     suit: 'diamonds',
-//     rank: 'ace'
-//   },
-//   {
-//     suit: 'diamonds',
-//     rank: 'king'
-//   },
-//   {
-//     suit: 'diamonds',
-//     rank: 'queen'
-//   },
-//   {
-//     suit: 'diamonds',
-//     rank: 'jack'
-//   },
-//   {
-//     suit: 'diamonds',
-//     rank: 10
-//   },
-//   {
-//     suit: 'diamonds',
-//     rank: 9
-//   },
-//   {
-//     suit: 'diamonds',
-//     rank: 8
-//   },
-//   {
-//     suit: 'diamonds',
-//     rank: 7
-//   },
-//   {
-//     suit: 'diamonds',
-//     rank: 6
-//   },
-//   {
-//     suit: 'diamonds',
-//     rank: 5
-//   },
-//   {
-//     suit: 'diamonds',
-//     rank: 4
-//   },
-//   {
-//     suit: 'diamonds',
-//     rank: 3
-//   },
-//   {
-//     suit: 'diamonds',
-//     rank: 2
-//   }
-// ];
+  {
+    suit: 'hearts',
+    rank: 'ace'
+  },
+  {
+    suit: 'hearts',
+    rank: 'king'
+  },
+  {
+    suit: 'hearts',
+    rank: 'queen'
+  },
+  {
+    suit: 'hearts',
+    rank: 'jack'
+  },
+  {
+    suit: 'hearts',
+    rank: 10
+  },
+  {
+    suit: 'hearts',
+    rank: 9
+  },
+  {
+    suit: 'hearts',
+    rank: 8
+  },
+  {
+    suit: 'hearts',
+    rank: 7
+  },
+  {
+    suit: 'hearts',
+    rank: 6
+  },
+  {
+    suit: 'hearts',
+    rank: 5
+  },
+  {
+    suit: 'hearts',
+    rank: 4
+  },
+  {
+    suit: 'hearts',
+    rank: 3
+  },
+  {
+    suit: 'hearts',
+    rank: 2
+  },
 
-// var cardKey = {
+  {
+    suit: 'diamonds',
+    rank: 'ace'
+  },
+  {
+    suit: 'diamonds',
+    rank: 'king'
+  },
+  {
+    suit: 'diamonds',
+    rank: 'queen'
+  },
+  {
+    suit: 'diamonds',
+    rank: 'jack'
+  },
+  {
+    suit: 'diamonds',
+    rank: 10
+  },
+  {
+    suit: 'diamonds',
+    rank: 9
+  },
+  {
+    suit: 'diamonds',
+    rank: 8
+  },
+  {
+    suit: 'diamonds',
+    rank: 7
+  },
+  {
+    suit: 'diamonds',
+    rank: 6
+  },
+  {
+    suit: 'diamonds',
+    rank: 5
+  },
+  {
+    suit: 'diamonds',
+    rank: 4
+  },
+  {
+    suit: 'diamonds',
+    rank: 3
+  },
+  {
+    suit: 'diamonds',
+    rank: 2
+  }
+];
+
+// // var cardKey = {
 
 //   ace: 11,
 //   king: 10,
@@ -251,3 +256,14 @@ console.log('Lodash is loaded:', typeof _ !== 'undefined');
 //   3: 3,
 //   2: 2
 // };
+
+function shuffle() {
+  var x;
+  // var cardKey;
+  for (var i = 0; i < players.length; i++) {
+    (_.shuffle(cards));
+    x = players[i].hand.push(Math.random(cards));
+  }
+  return x;
+}
+shuffle();

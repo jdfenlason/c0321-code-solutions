5a03b29c00ae9e1097aeb954d7923d9675baa62d
Jake <jdfenlason@gmail.com>
2021-04-21 17:57:29 -0400

    in progress of writing the deciding  winner function

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,269 +1,64 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
-// function cardGame (players, cards) {
-//   //shuffle
-// var _.shuffle
-// }
 
-var players = [
+// var players = [
+//   {
+//     name: 'Balder',
+//     hand: []
+//   },
+//   {
+//     name: 'Thor',
+//     hand: []
+//   },
 
-  {
-    name: 'Balder',
-    hand: []
-  },
-  {
-    name: 'Thor',
-    hand: []
-  },
+//   {
+//     name: 'Odin',
+//     hand: []
+//   },
 
-  {
-    name: 'Odin',
-    hand: []
-  },
+//   {
+//     name: 'Loki',
+//     hand: []
+//   }
+// ];
 
-  {
-    name: 'Loki',
-    hand: []
-  }
-];
-
-var cards = [
-  {
-    suit: 'spades',
-    rank: 'ace'
-  },
-  {
-    suit: 'spades',
-    rank: 'king'
-  },
-  {
-    suit: 'spades',
-    rank: 'queen'
-  },
-  {
-    suit: 'spades',
-    rank: 'jack'
-  },
-  {
-    suit: 'spades',
-    rank: 10
-  },
-  {
-    suit: 'spades',
-    rank: 9
-  },
-  {
-    suit: 'spades',
-    rank: 8
-  },
-  {
-    suit: 'spades',
-    rank: 7
-  },
-  {
-    suit: 'spades',
-    rank: 6
-  },
-  {
-    suit: 'spades',
-    rank: 5
-  },
-  {
-    suit: 'spades',
-    rank: 4
-  },
-  {
-    suit: 'spades',
-    rank: 3
-  },
-  {
-    suit: 'spades',
-    rank: 2
-  },
-
-  {
-    suit: 'clubs',
-    rank: 'ace'
-  },
-  {
-    suit: 'clubs',
-    rank: 'king'
-  },
-  {
-    suit: 'clubs',
-    rank: 'queen'
-  },
-  {
-    suit: 'clubs',
-    rank: 'jack'
-  },
-  {
-    suit: 'clubs',
-    rank: 10
-  },
-  {
-    suit: 'clubs',
-    rank: 9
-  },
-  {
-    suit: 'clubs',
-    rank: 8
-  },
-  {
-    suit: 'clubs',
-    rank: 7
-  },
-  {
-    suit: 'clubs',
-    rank: 6
-  },
-  {
-    suit: 'clubs',
-    rank: 5
-  },
-  {
-    suit: 'clubs',
-    rank: 4
-  },
-  {
-    suit: 'clubs',
-    rank: 3
-  },
-  {
-    suit: 'clubs',
-    rank: 2
-  },
-
-  {
-    suit: 'hearts',
-    rank: 'ace'
-  },
-  {
-    suit: 'hearts',
-    rank: 'king'
-  },
-  {
-    suit: 'hearts',
-    rank: 'queen'
-  },
-  {
-    suit: 'hearts',
-    rank: 'jack'
-  },
-  {
-    suit: 'hearts',
-    rank: 10
-  },
-  {
-    suit: 'hearts',
-    rank: 9
-  },
-  {
-    suit: 'hearts',
-    rank: 8
-  },
-  {
-    suit: 'hearts',
-    rank: 7
-  },
-  {
-    suit: 'hearts',
-    rank: 6
-  },
-  {
-    suit: 'hearts',
-    rank: 5
-  },
-  {
-    suit: 'hearts',
-    rank: 4
-  },
-  {
-    suit: 'hearts',
-    rank: 3
-  },
-  {
-    suit: 'hearts',
-    rank: 2
-  },
-
-  {
-    suit: 'diamonds',
-    rank: 'ace'
-  },
-  {
-    suit: 'diamonds',
-    rank: 'king'
-  },
-  {
-    suit: 'diamonds',
-    rank: 'queen'
-  },
-  {
-    suit: 'diamonds',
-    rank: 'jack'
-  },
-  {
-    suit: 'diamonds',
-    rank: 10
-  },
-  {
-    suit: 'diamonds',
-    rank: 9
-  },
-  {
-    suit: 'diamonds',
-    rank: 8
-  },
-  {
-    suit: 'diamonds',
-    rank: 7
-  },
-  {
-    suit: 'diamonds',
-    rank: 6
-  },
-  {
-    suit: 'diamonds',
-    rank: 5
-  },
-  {
-    suit: 'diamonds',
-    rank: 4
-  },
-  {
-    suit: 'diamonds',
-    rank: 3
-  },
-  {
-    suit: 'diamonds',
-    rank: 2
-  }
-];
-
-// // var cardKey = {
-
-//   ace: 11,
-//   king: 10,
-//   queen: 10,
-//   jack: 10,
-//   ten: 10,
-//   9: 9,
-//   8: 8,
-//   7: 7,
-//   6: 6,
-//   5: 5,
-//   4: 4,
-//   3: 3,
-//   2: 2
+// var cards = {
+//   rank: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'king', 'queen', 'ace'],
+//   suit: ['clubs', 'diamonds', 'hearts', 'spades']
 // };
 
-function shuffle() {
-  var x;
-  // var cardKey;
-  for (var i = 0; i < players.length; i++) {
-    (_.shuffle(cards));
-    x = players[i].hand.push(Math.random(cards));
-  }
-  return x;
-}
-shuffle();
+// function shuffleDeck() {
+//   var cardDeck = [];
+//   for (var i = 0; i < cards.suit.length; i++) {
+//     for (var x = 0; x < cards.rank.length; x++) {
+//       var shuffleCards = {};
+//       shuffleCards.rank = cards.rank[x];
+//       shuffleCards.suit = cards.suit[i];
+//       cardDeck.push(shuffleCards);
+//     }
+//   }
+//   var shuffledDeck = _.shuffle(cardDeck);
+//   return shuffledDeck;
+// }
+
+// function deal(playersArray, cardsPerHand, deck) {
+//   var hands = _.chunk(deck, cardsPerHand);
+//   for (var i = 0; i < playersArray.length; i++) {
+//     playersArray[i].hand = hands.shift();
+//   }
+// }
+
+// function handValues(playersArray, handSize) {
+//   for (var i = 0; i < playersArray.length; i++) {
+//     var cardValue = 0;
+//     for (var x = 0; x < handSize; x++) {
+//       if (playersArray[i].hand[x].rank === 'ace') {
+//         cardValue += 11;
+//       } else if (_.isString(playersArray[i].hand[x].rank)) {
+//         cardValue += 10;
+//       } else {
+//         cardValue += playersArray[i].hand[x].rank;
+//       }
+//       playersArray[i].value = cardValue;
+//     }
+//   }
+// }

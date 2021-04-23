0585079f02d07bb28f29f0aad27af5b174daa42c
Jake <jdfenlason@gmail.com>
2021-04-23 14:26:52 -0400

    finished object-manipulation-3

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,64 +1,79 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
+function Player(name) {
+  this.name = name;
+  this.playerHand = [];
+}
+function Deck() {
+  this.cards = [];
+}
 
-// var players = [
-//   {
-//     name: 'Balder',
-//     hand: []
-//   },
-//   {
-//     name: 'Thor',
-//     hand: []
-//   },
+Deck.prototype.createDeck = function (suits, ranks) {
+  suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+  ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'];
+  var value = [];
+  for (var i = 0; i < suits.length; i++) {
+    for (var k = 0; k < ranks.length; k++) {
+      if (ranks[k] === 'ace') {
+        value.push(11);
+      } else if (_.isString(ranks[k])) {
+        value.push(10);
+      } else {
+        value.push(ranks[k]);
+      }
+      this.cards.push((suits[i], ranks[k], value[k]));
+    }
+  }
+};
 
-//   {
-//     name: 'Odin',
-//     hand: []
-//   },
+Deck.prototype.deal = function (cardsPerHand) {
+  var deckHands = [];
+  while (deckHands.length < cardsPerHand) {
+    var shuffled = _.shuffle(this.cards);
+    deckHands.push(shuffled.pop(cardsPerHand));
+  }
+  return deckHands;
+};
 
-//   {
-//     name: 'Loki',
-//     hand: []
-//   }
-// ];
+// var deck = new Deck();
+// deck.createDeck();
+// console.log(deck.deal(4));
 
-// var cards = {
-//   rank: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'king', 'queen', 'ace'],
-//   suit: ['clubs', 'diamonds', 'hearts', 'spades']
-// };
+var gamePlayers = [
+  new Player('Odin'),
+  new Player('Thor'),
+  new Player('Loki'),
+  new Player('Balder')
+];
 
-// function shuffleDeck() {
-//   var cardDeck = [];
-//   for (var i = 0; i < cards.suit.length; i++) {
-//     for (var x = 0; x < cards.rank.length; x++) {
-//       var shuffleCards = {};
-//       shuffleCards.rank = cards.rank[x];
-//       shuffleCards.suit = cards.suit[i];
-//       cardDeck.push(shuffleCards);
-//     }
-//   }
-//   var shuffledDeck = _.shuffle(cardDeck);
-//   return shuffledDeck;
-// }
+function gameHandler(playersArray, cardsPerHand) {
+  var deck = new Deck();
+  deck.createDeck();
+  var score = [];
+  var counter = 0;
+  for (var i = 0; i < playersArray.length; i++) {
 
-// function deal(playersArray, cardsPerHand, deck) {
-//   var hands = _.chunk(deck, cardsPerHand);
-//   for (var i = 0; i < playersArray.length; i++) {
-//     playersArray[i].hand = hands.shift();
-//   }
-// }
+    playersArray[i].playerHand = (deck.deal(cardsPerHand));
+    counter = playersArray[i].playerHand.reduce(function (a, b) {
+      return a + b;
+    });
+    score.push(counter);
+  }
+  console.log(playersArray);
+  return playersArray[indexOfMax(score)].name;
+}
 
-// function handValues(playersArray, handSize) {
-//   for (var i = 0; i < playersArray.length; i++) {
-//     var cardValue = 0;
-//     for (var x = 0; x < handSize; x++) {
-//       if (playersArray[i].hand[x].rank === 'ace') {
-//         cardValue += 11;
-//       } else if (_.isString(playersArray[i].hand[x].rank)) {
-//         cardValue += 10;
-//       } else {
-//         cardValue += playersArray[i].hand[x].rank;
-//       }
-//       playersArray[i].value = cardValue;
-//     }
-//   }
-// }
+function indexOfMax(array) {
+  var max = array[0];
+  var maxIndex = 0;
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] > max) {
+      maxIndex = i;
+      max = array[i];
+    }
+  }
+  return maxIndex;
+}
+console.log(
+  'Winner Winner Chicken Dinner,',
+  gameHandler(gamePlayers, 5) + '!'
+);

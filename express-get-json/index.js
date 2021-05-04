257f36d8699432a5521c40075a7c7e94427d814b
Jake Fenlason <jdfenlason@gmail.com>
2021-05-04 01:29:12 +0000

    Completed express-get-json

---
+++ express-get-json/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.get('/api/grades', (req, res) => {
+  const newArr = [];
+  for (const objects in grades) {
+    newArr.push(grades[objects]);
+  }
+  res.json(newArr);
+});
+
+app.listen(3000, () => {
+
+});
